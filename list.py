bdb1cafd33f757a8d6a65e8b3defa4421f1a426a
shiv10vij <69182360+shiv10vij@users.noreply.github.com>
2020-08-04 09:33:41 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+mylist1 = [12,-7,5,64,-14]
+for number in mylist1:
+    if number<0:
+        mylist1.remove(number)
+        print(mylist1)
+        
+mylist2 = [12,14,-95,3]
+del mylist2[2]
+print(mylist2) 
